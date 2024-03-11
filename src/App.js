0ba4331ac38a6453a6c34f76a499cef3868ed501
Jacky <chongkinye@gmail.com>
2024-03-11 01:29:25 -0400

    first commit, 3d island can be shown

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+
+import { Home, About, Projects, Contact } from './pages'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='bg-slate-300/20'>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
