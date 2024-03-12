4550eff2fb7785248da22452a13e23a273ec4f13
Jacky <chongkinye@gmail.com>
2024-03-11 23:53:47 -0400

    added sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+import Sky from '../models/Sky'
+
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex 
 items-center justify-center'>
@@ -32,17 +34,19 @@ const Home = () => {
         <Canvas className='w-full h-screen bg-transparent'
         camera={{ near: 0.1, far: 1000 }}>
             <Suspense fallback={<Loader />}>
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <directionalLight position={[1, 1, 1]} intensity={2}/>
+                <ambientLight intensity={0.5}/>
+                {/* <pointLight />
+                <spotLight /> */}
+                <hemisphereLight skyColor='b1e1ff' groundColor='#000000'
+                intensity={1}/>
 
+                <Sky />
                 <Island
-									position={islandPosition}
-									scale={islandScale}
-									rotation={islandRotation}
-								/>
+                    position={islandPosition}
+                    scale={islandScale}
+                    rotation={islandRotation}
+                />
             </Suspense>
         </Canvas>
     </section>
